5678d9cbe0499f0541ffb37a505ca9a298f1b58d
Tyler Fedoris <tylerfedoris@outlook.com>
2020-09-25 22:22:27 -0400

    Changed name of Swagger UI Project and updated the definition to better reflect my personal work on the assignments. Re-exported the python-flask server stub files.

--- swagger_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.location_result import LocationResult
-from swagger_server.models.location_result_concordance import LocationResultConcordance
-from swagger_server.models.result import Result
-from swagger_server.models.result_concordance import ResultConcordance
