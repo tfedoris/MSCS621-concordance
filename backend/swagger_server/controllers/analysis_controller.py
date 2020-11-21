1f4eb54934c70ec32ed2977b30e29a40f847e89a
Tyler Fedoris <tylerfedoris@outlook.com>
2020-11-20 19:24:56 -0500

    Assignment 9: Reformatted analysis_controller using Black and created sonar-project properties file.

--- backend/swagger_server/controllers/analysis_controller.py
+++ backend/swagger_server/controllers/analysis_controller.py
@@ -1,19 +1,16 @@
-import decimal
-import json
+"""This module handles the logic for performing concordance analysis on a given body of text."""
 import operator
 import re
-from collections import Counter
 
 import boto3
+import botocore.exceptions
 import connexion
-import six
-from boto3.dynamodb.conditions import Key
-from swagger_server import util
-from swagger_server.database_methods import (ConcordanceTableOperations,
-                                             LocationsTableOperations)
+from swagger_server.database_methods import (
+    ConcordanceTableOperations,
+    LocationsTableOperations,
+)
 from swagger_server.models.location_result import LocationResult
-from swagger_server.models.location_result_concordance import \
-    LocationResultConcordance
+from swagger_server.models.location_result_concordance import LocationResultConcordance
 from swagger_server.models.result import Result
 from swagger_server.models.result_concordance import ResultConcordance
 
@@ -34,50 +31,49 @@ def get_concordance(body=None):  # noqa: E501
 
     try:
         ConcordanceTableOperations.create_concordance_table()
-    except:
+    except botocore.exceptions.ClientError:
+        print("Failed to communicate with DynamoDB, so it will not be used.\n")
         connected_to_database = False
 
     try:
         if connexion.request.is_json:
             body = str.from_dict(connexion.request.get_json())  # noqa: E501
-        
-        #initialize regular expression that excludes all punctuation and
-        #other special characters except for apostrophes and hyphens
-        regex = "[^a-zA-Z'\- ]+"
-        
-        input = body.decode('utf-8')
+
+        # initialize regular expression that excludes all punctuation and
+        # other special characters except for apostrophes and hyphens
+        regex = r"[^a-zA-Z'- ]+"
+
+        input_text = body.decode("utf-8")
         concordance = []
 
         if connected_to_database:
-            dynamodb_resource = boto3.resource('dynamodb', region_name='us-east-2')
-            table = dynamodb_resource.Table('concordance')
-            response = table.get_item(
-                Key={
-                    'input': input
-                }
-            )
+            dynamodb_resource = boto3.resource("dynamodb", region_name="us-east-2")
+            table = dynamodb_resource.Table("concordance")
+            response = table.get_item(Key={"input": input_text})
 
-            if 'Input' in response:
-                concordance = response['input']['concordance']
+            if "Input" in response:
+                concordance = response["input"]["concordance"]
 
-        else:  
-            #parse only alpha characters using the initialized regular expression
-            input_split = re.sub(regex, '', input.lower())
+        else:
+            # parse only alpha characters using the initialized regular expression
+            input_split = re.sub(regex, "", input_text.lower())
             input_split = input_split.split()
-            
+
             used_words = []
             for word in input_split:
-                if (used_words.count(word) == 0):
+                if used_words.count(word) == 0:
                     concordance.append(ResultConcordance(word, input_split.count(word)))
                     used_words.append(word)
-        
-        concordance.sort(key=operator.attrgetter('token'))
-        
-    except Exception as error:
+
+        concordance.sort(key=operator.attrgetter("token"))
+
+    # general exception is used here as a 'catch-all'
+    # concordance should return an empty result if it failed regardless of the error
+    except Exception:
         concordance = []
         code = 400
 
-    result = Result(concordance, input)
+    result = Result(concordance, input_text)
 
     if connected_to_database:
         ConcordanceTableOperations.upload_concordance_data(result)
@@ -101,54 +97,54 @@ def get_concordance_with_location(body=None):  # noqa: E501
 
     try:
         LocationsTableOperations.create_location_table()
-    except:
+    except botocore.exceptions.ClientError:
+        print("Failed to communicate with DynamoDB, so it will not be used.\n")
         connected_to_database = False
-    
+
     try:
         if connexion.request.is_json:
             body = str.from_dict(connexion.request.get_json())  # noqa: E501
-            
-        #initialize regular expression that excludes all punctuation and
-        #other special characters except for apostrophes and hyphens
-        regex = "[^a-zA-Z'\- ]+"
-        
-        input = body.decode('utf-8')
+
+        # initialize regular expression that excludes all punctuation and
+        # other special characters except for apostrophes and hyphens
+        regex = r"[^a-zA-Z'- ]+"
+
+        input_text = body.decode("utf-8")
         concordance = []
 
         if connected_to_database:
-            dynamodb_resource = boto3.resource('dynamodb', region_name='us-east-2')            
-            table = dynamodb_resource.Table('locations')
-            response = table.get_item(
-                Key={
-                    'input': input
-                }
-            )
+            dynamodb_resource = boto3.resource("dynamodb", region_name="us-east-2")
+            table = dynamodb_resource.Table("locations")
+            response = table.get_item(Key={"input": input_text})
 
-            if 'Input' in response:
-                concordance = response['input']['concordance']
-        
+            if "Input" in response:
+                concordance = response["input"]["concordance"]
+
         else:
-            #parse only alpha characters using the initialized regular expression
-            input_split = re.sub(regex, '', input.lower())
+            # parse only alpha characters using the initialized regular expression
+            input_split = re.sub(regex, "", input_text.lower())
             input_split = input_split.split()
-            
+
             used_words = []
             for word in input_split:
-                if (used_words.count(word) == 0):
-                    locations = [i for i in range(len(input_split)) if input_split[i] == word]
+                if used_words.count(word) == 0:
+                    locations = [
+                        i for i in range(len(input_split)) if input_split[i] == word
+                    ]
                     concordance.append(LocationResultConcordance(word, locations))
                     used_words.append(word)
-        
-        concordance.sort(key=operator.attrgetter('token'))
-    
-    except Exception as error:
+
+        concordance.sort(key=operator.attrgetter("token"))
+
+    # general exception is used here as a 'catch-all'
+    # concordance should return an empty result if it failed regardless of the error
+    except Exception:
         concordance = []
         code = 400
 
-    result = LocationResult(concordance, input)
-
+    result = LocationResult(concordance, input_text)
 
     if connected_to_database:
         LocationsTableOperations.upload_location_data(result)
-    
+
     return result, code
